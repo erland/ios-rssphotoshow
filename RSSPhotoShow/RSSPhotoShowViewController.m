4c03216cec3ef15559bf10b97ef0e578a01c0e2f
Erland Isaksson <erland@Erlands-MacBook-Pro.local>
2012-04-15 10:42:41 +0200

    Added swipe support Added toolbar at the bottom Shuffled images Removed status bar Adjusted the layout a bit to allow full screen images

--- RSSPhotoShow/RSSPhotoShowViewController.m
+++ RSSPhotoShow/RSSPhotoShowViewController.m
@@ -13,19 +13,27 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *creditLabel;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
 @property (strong, nonatomic) NSArray * photos;
 @property (nonatomic) NSInteger currentPhoto;
 @property (weak, nonatomic) Photo *photo;
 @property (strong, nonatomic) NSTimer * timer;
+@property (strong, nonatomic) UITapGestureRecognizer *imageViewTouchRecognizer;
+@property (strong, nonatomic) UISwipeGestureRecognizer *imageViewSwipeLeftRecognizer;
+@property (strong, nonatomic) UISwipeGestureRecognizer *imageViewSwipeRightRecognizer;
 @end
 
 @implementation RSSPhotoShowViewController
 @synthesize imageView = _imageView;
 @synthesize titleLabel = _titleLabel;
 @synthesize creditLabel = _creditLabel;
+@synthesize toolbar = _toolbar;
 @synthesize photos = _photos;
 @synthesize currentPhoto = _currentPhoto;
 @synthesize timer = _timer;
+@synthesize imageViewTouchRecognizer = _imageViewTouchRecognizer;
+@synthesize imageViewSwipeLeftRecognizer = _imageViewSwipeLeftRecognizer;
+@synthesize imageViewSwipeRightRecognizer = _imageViewSwipeRightRecognizer;
 
 - (Photo*)photo {
     if(self.currentPhoto && self.photos && self.photos.count>self.currentPhoto && self.currentPhoto>=0) {
@@ -35,6 +43,7 @@
     }
 }
 - (void)setPhoto:(Photo*)photo {
+    [self.timer invalidate];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSData *imageData = [NSData dataWithContentsOfURL:photo.url];
         dispatch_async(dispatch_get_main_queue(),^{
@@ -57,18 +66,37 @@
                                                           self.creditLabel.alpha = 1;
                                                       }
                                                       completion:^(BOOL finished) {
-                                                          self.timer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(switchImage) userInfo:nil repeats:NO];
+                                                          self.timer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(switchImageThroughTimer:) userInfo:nil repeats:NO];
                                                       }];
                                  }
                              }];
         });
     });
 }
+
+
+-(NSArray *)shuffledArray:(NSArray*)array;
+{
+    
+    NSMutableArray *shuffledArray = [NSMutableArray arrayWithCapacity:[array count]];
+    
+    NSMutableArray *copy = [array mutableCopy];
+    while ([copy count] > 0)
+    {
+        int index = arc4random() % [copy count];
+        id objectToMove = [copy objectAtIndex:index];
+        [shuffledArray addObject:objectToMove];
+        [copy removeObjectAtIndex:index];
+    }
+    
+    return shuffledArray;
+}
+
 - (void)retrievePhotos {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSArray *newPhotos = [[[RSSPhotoFetcher alloc]init] fetchWithURL:[NSURL URLWithString:@"http://feeds.feedburner.com/seanreiser/flickrinterestingness?format=xml"]];
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.photos = newPhotos;
+            self.photos = [self shuffledArray:newPhotos];
         });
     });
 }
@@ -76,12 +104,18 @@
 - (void)setPhotos:(NSArray*)photos {
     _photos = photos;
     self.currentPhoto = -1;
-    [self switchImage];
+    [self switchImage:1];
 }
-- (void)switchImage {
+
+
+- (void)switchImageThroughTimer:(NSTimer*)theTimer {
+    [self switchImage:1];
+}
+
+- (void)switchImage:(NSInteger)increment {
     if(self.photos && self.photos.count>0) {
-        self.currentPhoto++;
-        if(self.currentPhoto >= self.photos.count) {
+        self.currentPhoto+=increment;
+        if((int)self.currentPhoto >= (int)self.photos.count) {
             self.currentPhoto = 0;
         }else if(self.currentPhoto<0) {
             self.currentPhoto = self.photos.count-1;
@@ -91,24 +125,56 @@
 }
 
 - (void)viewDidAppear:(BOOL)animated {
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:NO];
     [super viewDidAppear:animated];
     [self retrievePhotos];
     if(self.photos && self.photos.count>0) {
-        [self switchImage];
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:15 target:self selector:@selector(switchImage) userInfo:nil repeats:YES];
+        [self switchImage:1];
     }else {
         self.titleLabel.text = @"Loading...";
         self.creditLabel.text = @"";
     }
     [[UIApplication sharedApplication] setIdleTimerDisabled: YES];
+    self.imageViewTouchRecognizer = 
+    [[UITapGestureRecognizer alloc] initWithTarget:self 
+                                            action:@selector(imageViewWasTouched:)];
+    [self.imageView addGestureRecognizer:self.imageViewTouchRecognizer];
+
+    self.imageViewSwipeLeftRecognizer = 
+    [[UISwipeGestureRecognizer alloc] initWithTarget:self 
+                                            action:@selector(nextImage:)];
+    self.imageViewSwipeLeftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    [self.imageView addGestureRecognizer:self.imageViewSwipeLeftRecognizer];
+
+    self.imageViewSwipeRightRecognizer = 
+    [[UISwipeGestureRecognizer alloc] initWithTarget:self 
+                                              action:@selector(previousImage:)];
+    self.imageViewSwipeRightRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.imageView addGestureRecognizer:self.imageViewSwipeRightRecognizer];
+}
+- (IBAction)nextImage:(id)sender {
+    [self switchImage:1];
+}
+- (IBAction)previousImage:(id)sender {
+    [self switchImage:-1];
 }
 
+- (void)imageViewWasTouched:(id)sender {
+    if(self.toolbar.hidden) {
+        self.toolbar.hidden = NO;
+    }else {
+        self.toolbar.hidden = YES;
+    }
+}
 
 - (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
     [self.timer invalidate];
     self.timer = nil;
     [[UIApplication sharedApplication] setIdleTimerDisabled: NO];
+    [self.imageView removeGestureRecognizer:self.imageViewTouchRecognizer];
+    [self.imageView removeGestureRecognizer:self.imageViewSwipeLeftRecognizer];
+    [self.imageView removeGestureRecognizer:self.imageViewSwipeRightRecognizer];
+    [super viewDidDisappear:animated];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -120,6 +186,7 @@
     [self setImageView:nil];
     [self setTitleLabel:nil];
     [self setCreditLabel:nil];
+    [self setToolbar:nil];
     [super viewDidUnload];
 }
 @end
